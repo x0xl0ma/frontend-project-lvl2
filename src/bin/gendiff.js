3d17ab57d3c05e469a4dab01ea765638545ac5fd
Vladislav <k0tjaka@yandex.ru>
2019-12-12 20:26:01 +0300

    second step

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,4 +5,9 @@ import program from 'commander';
 program
   .version('1.0.0','-V, --version', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
-program.parse(process.argv);
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstPath, secondPath) => {
+    concole.log(genDiff(firstPath, secondPath));
+})
+.parse(process.argv);
