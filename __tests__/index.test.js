a75930f5a65a65045561d6ee04da9c380700f068
Vladislav <k0tjaka@yandex.ru>
2019-12-19 23:01:06 +0300

    step 4.3

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../src';
+
+describe('genDiff', () => {
+  test('get the difference', () => {
+    const pathToResult = path.join(__dirname, '/__fixtures__/result');
+    const pathToFile1 = path.join(__dirname, '/__fixtures__/before.json');
+    const pathToFile2 = path.join(__dirname, '/__fixtures__/after.json');
+    const received = genDiff(pathToFile1, pathToFile2);
+    const expected = fs.readFileSync(pathToResult, 'utf-8');
+    expect(received).toBe(expected);
+  });
+});
