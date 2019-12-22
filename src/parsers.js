f2755db5ba9a4a31d79ad926c0070caf25e07b5b
Vladislav <k0tjaka@yandex.ru>
2019-12-22 22:20:14 +0300

    step 6

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,11 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
+
 
 const parsers = {
   '.json': (data) => JSON.parse(data),
   '.yaml': (data) => yaml.safeLoad(data),
+  '.ini': (data) => ini.parse(data),
 };
 
 const parse = (data, type) => parsers[type](data);
