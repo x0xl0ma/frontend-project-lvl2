501bf85c77768f80cf69dd835ed3b11be1240540
Vladislav <k0tjaka@yandex.ru>
2019-12-28 23:27:49 +0300

    step 6. test failed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,15 +11,13 @@ const formats = {
 
 const getFormat = (type) => formats[type];
 
-const fixuturesPath = path.join(__dirname, '__fixtures__');
-
 test.each([
   ['before', 'after', 'json'],
   ['before', 'after', 'yaml'],
   ['before', 'after', 'ini'],
 ])('get the Diffirence. s%', () => {
-  const pathToFile1 = path.join(fixuturesPath, `before.${getFormat}`);
-  const pathToFile2 = path.join(fixuturesPath, `after.${getFormat}`);
+  const pathToFile1 = `${__dirname}/__fixtures__/before.${getFormat}`;
+  const pathToFile2 = `${__dirname}/__fixtures__/after.${getFormat}`;
   const pathToResult = path.join(__dirname, '/__fixtures__/result');
   const expected = fs.readFileSync(pathToResult, 'utf-8');
   expected(genDiff(pathToFile1, pathToFile2)).toEqual(expected);
