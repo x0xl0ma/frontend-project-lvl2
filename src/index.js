f76b641bab4c8ae69dd9d486b75ebd1b632e597a
Vladislav <k0tjaka@yandex.ru>
2020-01-01 20:44:54 +0300

    step 7. sad

--- src/index.js
+++ src/index.js
@@ -3,30 +3,70 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
 
-const genDiff = (pathToFile1, pathToFile2) => {
-  const data1 = fs.readFileSync(pathToFile1, 'utf-8');
-  const data2 = fs.readFileSync(pathToFile2, 'utf-8');
+const parsing = (pathToFile) => {
+  const data = fs.readFileSync(pathToFile, 'utf-8');
+  const fileType = path.extname(pathToFile);
+  const obj = parse(data, fileType);
+  return obj;
+};
 
-  const type1 = path.extname(pathToFile1);
-  const type2 = path.extname(pathToFile2);
-
-  const obj1 = parse(data1, type1);
-  const obj2 = parse(data2, type2);
-
+const treeBuilder = (obj1, obj2) => {
   const keys = union(Object.keys(obj1), Object.keys(obj2)).sort();
 
-  const result = keys.map((key) => {
-    if (!has(obj2, key)) {
-      return (` - ${key}: ${obj1[key]}`);
+  const tree = keys.map((key) => {
+    if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
+      return {
+        key,
+        type: 'nested',
+        children: treeBuilder(obj1[key], obj2[key]),
+      };
     }
-    if (!has(obj1, key)) {
-      return (` + ${key}: ${obj2[key]}`);
+    if (!has(obj2, key) && has(obj1, key)) {
+      return {
+        key,
+        type: 'removed',
+        value: obj1[key],
+      };
+    }
+    if (!has(obj1, key) && has(obj2, key)) {
+      return {
+        key,
+        type: 'added',
+        value: obj2[key],
+      };
     }
     if (obj1[key] !== obj2[key]) {
-      return (` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`);
+      return {
+        key,
+        type: 'changed',
+        valueBefore: obj1[key],
+        valueAfter: obj2[key],
+      };
     }
-    return (`   ${key}: ${obj2[key]}`);
-  }).join('\n');
-  return `{\n${result}\n}`;
+    return {
+      key,
+      type: 'unchanged',
+      value: obj1[key],
+    };
+  });
+  return tree;
+};
+
+const getDataFromType = {
+  removed: (node) => ` - ${node.key}: ${node.value}`,
+  added: (node) => ` + ${node.key}: ${node.value}`,
+  changed: (node) => ` - ${node.key}: ${node.valueBefore}\n + ${node.key}: ${node.valueAfter}`,
+  unchanged: (node) => `  ${node.key}: ${node.value}`,
+  nested: (node) => mapping(node),
+};
+
+const mapping = (tree) => tree.map((node) => getDataFromType[node.type](node)).join('\n');
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const object1 = parsing(pathToFile1);
+  const object2 = parsing(pathToFile2);
+  const ast = treeBuilder(object1, object2);
+  const mappedAst = mapping(ast);
+  return mappedAst;
 };
 export default genDiff;
